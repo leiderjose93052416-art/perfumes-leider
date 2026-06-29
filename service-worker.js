12b982de62061f22963ccc4ce6dcc280736bfe9b
leiderjose93052416-art <leider.jose93052416@gmail.com>
2026-06-29 16:47:51 -0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = "perfumes-leider-v1";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  // No interceptar peticiones por ahora.
+});
